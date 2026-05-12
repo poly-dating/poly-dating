76141f0801a6619e89663600450e33e6eda36197
HRlucy <he2089@naver.com>
2026-05-12 14:40:06 +0900

    feat: Event 인터페이스 추가

--- src/main/java/com/polydating/event/Event.java
+++ src/main/java/com/polydating/event/Event.java
@@ -1,4 +1,10 @@
 package com.polydating.event;
 
-public class Event {
+import com.polydating.character.Character;
+import com.polydating.state.GameState;
+
+public interface Event {
+    String getTitle();
+
+    void apply(Character target, GameState state);
 }
