5c507959885bb2ad1bf6acf99107c3d7c207b673
HRlucy <he2089@naver.com>
2026-05-12 14:48:15 +0900

    feat: Efix 인터페이스 추가

--- src/main/java/com/polydating/event/Event.java
+++ src/main/java/com/polydating/event/Event.java
@@ -3,8 +3,10 @@ package com.polydating.event;
 import com.polydating.character.Character;
 import com.polydating.state.GameState;
 
+import java.util.Scanner;
+
 public interface Event {
     String getTitle();
 
-    void apply(Character target, GameState state);
+    void apply(Character target, GameState gameState, Scanner scanner);
 }
